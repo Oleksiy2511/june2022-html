df5c07c5414598f23c779f4818a873770cfe463e
USERoleksiy#87741 <kurulenko275@gmail.com>
2022-07-22 14:05:00 +0300

    hello.JS hw-1.

---
+++ JS-HW/hw-js-1/hw-1-js.js
@@ -0,0 +1,50 @@
+let hello = 'hello';
+alert(hello)
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+alert(ua)
+let num1 = 1;
+let num10 = 10;
+let num9 = -999;
+let num123 = 123;
+let pi = 3.14;
+let num2 = 2.7;
+let num16 = 16;
+let bool1 = true;
+let bool2 = false;
+console.log(hello, owu, com, ua, num1, num10, num9, num123, pi, num2, num16, bool1, bool2);
+document.write(`${hello} ${owu} ${com} ${ua} ${num1} ${num10} ${num9} ${num123} ${pi} ${num2} ${num16} ${bool1} ${bool2}`)
+
+////////////////////////////////////////////////////
+
+// let firstName='Олексій';
+// let middleName='Олексійович';
+// let lastName='Кириленко'
+// let person=lastName+firstName+middleName;
+// let person2=lastName+' '+firstName+' '+middleName;
+// console.log(person);
+// console.log(person2);
+// document.write(person)
+// document.write(person2)
+
+/////////////////////////////////////////////
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+///////////////////////////////////////////////////////
+
+// let firstName2=prompt('ім`я');
+// let middleName2=prompt('по батькові');
+// let age=+prompt('ваш вік');
+// let greeting=firstName2+middleName2+age;
+// console.log(greeting);
+// console.log('вітаю '+firstName2+' '+middleName2+'. вам '+age+' років!!')
+// console.log(`hello ${firstName2} ${middleName2}. you ${age} year old`)
+// document.write(greeting);
+// document.write('вітаю '+firstName2+' '+middleName2+'. вам '+age+' років!!')
+// document.write(`hello ${firstName2} ${middleName2}. you ${age} year old`)
